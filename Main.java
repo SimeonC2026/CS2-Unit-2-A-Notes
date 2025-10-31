5a7368f2fdca184920af45c335f4e224624bde6e
SimeonC2026 <simeon.chew2026@gbwl.org>
2025-10-31 12:56:45 +0000

    Halloween Coding!

--- Main.java
+++ Main.java
@@ -1,6 +1,37 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      
+      // BOOLEAN VARIABLES can only hold true or false
+      boolean lightsOn = false;
+      boolean dannyPresent = true;
+      
+      // BOOLEAN EXPRESSIONS are like questions
+      // where the only possible answers are true or false
+      boolean exp1 = (dannyPresent == true); // is Danny here?
+      boolean epx2 = (lightsOn != true); // are the lights NOT on? (off)
+
+      // Comparing numerical values
+      int x = 6;
+      int y = 7;
+
+      // Check for EQUALITY using the == operator
+      System.out.println( x == y); // you can print boolean exp. directly
+      // Check for NOT equal using the != operator
+      boolean exp3 = (x != y); // store the boolean exp result in a variable
+      System.out.println( exp3 );
+
+      // Compare numeric values (int, double) using RELATIONAL OPERATORS
+      System.out.println( x < y ); // true (6 IS LESS THAN 7 )
+      System.out.println( x > y); // false
+      System.out.println( x <= y ); // true
+      System.out.println( x >= y ); // false
+      System.out.println( y >= x); // true (ORDER MATTERS w/rel. operators)
+      System.out.println( y == y ); // true
+
+      // Can you compare ints and doubles?
+      double z = 6.0;
+      System.out.println( x == z ); // true (6 and 6.0 are the same value!)
+      
    }
 }
